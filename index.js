693e4566a635cae1cd2dd5b93d6a4bbc6250a431
febiolagumay <febiolagumay@gmail.com>
2024-12-18 21:26:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// // const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-// "/"];
   
   let div1 = document.getElementById("div-1")
   let div2 = document.getElementById("div-2")
@@ -16,9 +14,6 @@ function makePasswd() {
   return passwd;
 }
 
-
-
-// div2.textContent = passwd()
 
 function handleclick() {
     div1.textContent = makePasswd()
